a99d537c35f2040566939bf81651d76a557e598c
Rubin <rubinradhakrishnan@gmail.com>
2020-06-15 17:31:42 +0530

    Set boundaries for the character and Added Jump feature

--- firstgame.py
+++ firstgame.py
@@ -7,10 +7,13 @@ win = pygame.display.set_mode((500, 500)) #window size
 pygame.display.set_caption("First Game") #window name
 
 x = 50
-y = 50
+y = 425
 width = 40
 height = 60
 vel = 5
+
+isJump = False
+jumpCount = 10
 
 run = True
 while(run):
@@ -24,14 +27,28 @@ while(run):
 
     # Navigating the character
 
-    if keys[pygame.K_LEFT]:
+    if keys[pygame.K_LEFT] and x > vel:
         x -= vel
-    if keys[pygame.K_RIGHT]:
+    if keys[pygame.K_RIGHT] and x < 500 - width - vel: # Restricting within window along x-axis
         x += vel
-    if keys[pygame.K_UP]:
-        y -= vel
-    if keys[pygame.K_DOWN]:
-        y += vel
+
+    if not(isJump):
+        if keys[pygame.K_UP] and y > vel:
+            y -= vel
+        if keys[pygame.K_DOWN] and y < 500 - height -vel: # Restricting within window along y-axis
+            y += vel
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            neg = 1
+            if jumpCount < 0:
+                neg = -1
+            y -= (jumpCount ** 2) * 0.5 * neg #to bring the character down
+            jumpCount -= 1
+        else:
+            isJump = False
+            jumpCount = 10
 
     win.fill((0, 0, 0))
     # Drawing the character
