abe5a15bb6090c765550fb4de650aa3249e054e9
Rubin <rubinradhakrishnan@gmail.com>
2020-06-15 19:34:08 +0530

    Optimized the Code and made it Object Oriented

--- firstgame.py
+++ firstgame.py
@@ -11,82 +11,87 @@ walkLeft = [pygame.image.load('sprites/L1.png'), pygame.image.load('sprites/L2.p
 bg = pygame.image.load('sprites/bg.jpg')
 char = pygame.image.load('sprites/standing.png')
 
-clock  = pygame.time.Clock()
 
-x = 50
-y = 400
-width = 64
-height = 64
-vel = 5
+clock = pygame.time.Clock()
 
-isJump = False
-jumpCount = 10
 
-left = False
-right = False
-walkCount = 10
+class player(object):
+    def __init__(self,x,y,width,height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 5
+        self.isJump = False
+        self.left = False
+        self.right = False
+        self.walkCount = 0
+        self.jumpCount = 10
+
+    def draw(self, win):
+        if self.walkCount + 1 >= 27:
+            self.walkCount = 0
+
+        if self.left:
+            win.blit(walkLeft[self.walkCount//3], (self.x,self.y))
+            self.walkCount += 1
+        elif self.right:
+            win.blit(walkRight[self.walkCount//3], (self.x,self.y))
+            self.walkCount +=1
+        else:
+            win.blit(char, (self.x,self.y))
+
+
 
 def redrawGameWindow():
-    global walkCount
-    win.blit(bg, (0, 0)) #filling background image
+    win.blit(bg, (0,0))
+    man.draw(win)
     
-    if walkCount + 1 >= 27:
-        walkCount = 0
+    pygame.display.update()
 
-    if left:
-        win.blit(walkLeft[walkCount//3], (x, y))
-        walkCount += 1
-    elif right:
-        win.blit(walkRight[walkCount//3], (x, y))
-        walkCount += 1
-    else:
-        win.blit(char, (x, y))
-
-    pygame.display.update() # Refresh the window
 
 #mainloop
+man = player(200, 410, 64,64)
 run = True
-while(run):
+while run:
     clock.tick(27)
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: # Enabling the close button
+        if event.type == pygame.QUIT:
             run = False
 
     keys = pygame.key.get_pressed()
 
-    # Navigating the character
-
-    if keys[pygame.K_LEFT] and x > vel:
-        x -= vel
-        left = True
-        right = False
-    elif keys[pygame.K_RIGHT] and x < 500 - width - vel: # Restricting within window along x-axis
-        x += vel
-        right = True
-        left = False
+    if keys[pygame.K_LEFT] and man.x > man.vel:
+        man.x -= man.vel
+        man.left = True
+        man.right = False
+    elif keys[pygame.K_RIGHT] and man.x < 500 - man.width - man.vel:
+        man.x += man.vel
+        man.right = True
+        man.left = False
     else:
-        right = False
-        left = False
-        walkCount = 0
-
-    if not(isJump):
+        man.right = False
+        man.left = False
+        man.walkCount = 0
+        
+    if not(man.isJump):
         if keys[pygame.K_SPACE]:
-            isJump = True
-            right = False
-            left = False
-            walkCount = 0
+            man.isJump = True
+            man.right = False
+            man.left = False
+            man.walkCount = 0
     else:
-        if jumpCount >= -10:
+        if man.jumpCount >= -10:
             neg = 1
-            if jumpCount < 0:
+            if man.jumpCount < 0:
                 neg = -1
-            y -= (jumpCount ** 2) * 0.5 * neg #to bring the character down
-            jumpCount -= 1
+            man.y -= (man.jumpCount ** 2) * 0.5 * neg
+            man.jumpCount -= 1
         else:
-            isJump = False
-            jumpCount = 10
-
+            man.isJump = False
+            man.jumpCount = 10
+            
     redrawGameWindow()
 
 pygame.quit()
